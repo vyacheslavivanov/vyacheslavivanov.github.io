f4505c46880e09294e0afd778f698dd0ef8675f1
vyacheslavivanov <vsivanov99@gmail.com>
2020-11-15 22:36:23 +0300

    Utopia

--- js/script.js
+++ js/script.js
@@ -1,80 +1,17 @@
 $(document).ready(function(){
-
-	$('.disactive-on-load').removeClass("active");
-
-	$("#phone").mask("+9 (999) 99-99-999");
-
-$('.popup-show')
-	.click(function(){
-		$('.popup-container').show();
-	});	
-
-
-$('.popup__close')
-	.click(function(){
-		$('.popup-container').hide();
-		$('.popup__more').hide();
-	});
-
-$('.more__img')
-	.click(function(){
-		$('.popup__more').slideToggle();
-		$('.more__img').toggleClass('more__transform');
-	});
-
-
-$('.popup-container')
-	.click(function(event){
-		if(event.target == this) {
-			$(this).hide();
-			$('.popup__more').hide();
-		}
-	});
-
-$(function(){
-	$('#form').validate({
-		 rules: {
-		 fio: {
-			 required: true,
-			 minlength: 2
-		 },
-		 tel: 'required',
-		 email: {
-			 email: true,
-		 }
-		 },
-		 messages: {
-		 fio: {
-		 required: "Необходимо ввести ФИО",
-		 minlength: "Введите не менее 2-х символов в поле 'Имя'"
-		 },
-		 email: {
-			 required: "Введите e-mail",
-		 email: "Адрес должен быть вида name@domain.com"
-			},
-		 tel: {
-		 required: "Необходимо ввести телефон"}
-		},
-		submitHandler: function(){
-			sendAjaxForm();
-			return false; 
-		}
-	});
-
-
-		// function sendAjaxForm() {
-		// 	$.ajax({
-		// 		type: "POST",
-		// 		url: "mail.php",
-		// 		data: $("#form").serialize()
-		// 		}).done(function() {
-		// 		$('.popup-container').hide();
-		// 		$('.success-container').addClass('flex');
-		// 			setTimeout("$('.success-container').removeClass('flex')", 2000);
-		// 			$("#form").trigger("reset");
-		// 	});
-		// }
-	});
+	$(window).on('scroll', function() {
+		let $headerLogo = $('.header__logo'),
+				$headerItem = $('.item-a')
+				scroll = $(this).scrollTop(),
+				headerHeight = $('.header').height() - 100;
+    if (scroll > headerHeight) {
+				$headerLogo.addClass('header__logo_black'),
+				$headerItem.addClass('item-a-black');
+    } else if (scroll < headerHeight) {
+				$headerLogo.removeClass('header__logo_black'),
+				$headerItem.removeClass('item-a-black');
+    }
+});
 });
 
   
