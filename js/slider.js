e969ead35d3512fb27eb6dda6ffbf7d76974b32c
vyacheslavivanov <vsivanov99@gmail.com>
2021-06-03 20:07:28 +0300

    laguna commit

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: 'true'
+  }
+});
+
